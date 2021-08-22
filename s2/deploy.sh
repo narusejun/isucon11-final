6cb26b2936947d5e0e5f7cd3eb08a9fcf938635d
wtks <30363887+wtks@users.noreply.github.com>
2021-08-22 20:16:22 +0900

    ディレクトリ作成

---
+++ s2/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eux
+
+# sudo cp -f etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+# sudo cp -f etc/nginx/nginx.conf /etc/nginx/nginx.conf
+# sudo nginx -t
+# sudo cp -f etc/nginx/sites-available/isucondition.conf /etc/nginx/sites-available/isucondition.conf
+# sudo cp -f home/isucon/env.sh /home/isucon/env.sh
+
+# cd /home/isucon/webapp/go
+# go build -o isucondition
+
+# sudo systemctl restart isucondition.go
+# sudo systemctl restart nginx
+# sudo systemctl restart mariadb
+
+
+# slow query logを有効化する
+# QUERY="
+#  set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
+#  set global long_query_time = 0;
+#  set global slow_query_log = ON;
+# "
+#
+# echo $QUERY | sudo mysql -uroot
+
+# log permission
+sudo chmod 777 /var/log/nginx /var/log/nginx/*
+sudo chmod 777 /var/log/mysql /var/log/mysql/*
