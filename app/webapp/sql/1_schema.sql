be066473e9db2ae6b379721e4cd73fe91bf51226
wtks <30363887+wtks@users.noreply.github.com>
2021-09-18 11:16:14 +0900

    index追加

--- app/webapp/sql/1_schema.sql
+++ app/webapp/sql/1_schema.sql
@@ -81,5 +81,6 @@ CREATE TABLE `unread_announcements`
     `is_deleted`      TINYINT(1) NOT NULL DEFAULT false,
     PRIMARY KEY (`announcement_id`, `user_id`),
     CONSTRAINT FK_unread_announcements_announcement_id FOREIGN KEY (`announcement_id`) REFERENCES `announcements` (`id`),
-    CONSTRAINT FK_unread_announcements_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+    CONSTRAINT FK_unread_announcements_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    INDEX user_id__is_deleted (user_id, is_deleted)
 );
