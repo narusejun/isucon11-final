be665b513a837ce541db5f991044fbda71c4a8fd
wtks <30363887+wtks@users.noreply.github.com>
2021-09-18 17:30:09 +0900

    slow log off

--- s3/deploy.sh
+++ s3/deploy.sh
@@ -15,12 +15,12 @@ sudo systemctl restart mysql
 
 
 # slow query logを有効化する
-QUERY="
- set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
- set global long_query_time = 0;
- set global slow_query_log = ON;
-"
-echo $QUERY | sudo mysql -uroot
+#QUERY="
+# set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
+# set global long_query_time = 0;
+# set global slow_query_log = ON;
+#"
+#echo $QUERY | sudo mysql -uroot
 
 # log permission
 sudo chmod 777 /var/log/nginx /var/log/nginx/*
