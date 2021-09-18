37ed373ed34d32815e118b2ce73c54575219f3ae
wtks <30363887+wtks@users.noreply.github.com>
2021-09-18 12:01:47 +0900

    unreadをバルクインサート

--- app/webapp/go/main.go
+++ app/webapp/go/main.go
@@ -1397,11 +1397,20 @@ func (h *handlers) AddAnnouncement(c echo.Context) error {
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	for _, user := range targets {
-		if _, err := tx.Exec("INSERT INTO `unread_announcements` (`announcement_id`, `user_id`) VALUES (?, ?)", req.ID, user.ID); err != nil {
-			c.Logger().Error(err)
-			return c.NoContent(http.StatusInternalServerError)
+	var sb strings.Builder
+	for i, target := range targets {
+		sb.WriteString("('")
+		sb.WriteString(req.ID)
+		sb.WriteString("','")
+		sb.WriteString(target.ID)
+		sb.WriteString("')")
+		if i < len(targets)-1 {
+			sb.WriteString(",")
 		}
+	}
+	if _, err := tx.Exec("INSERT INTO `unread_announcements` (`announcement_id`, `user_id`) VALUES " + sb.String()); err != nil {
+		c.Logger().Error(err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	if err := tx.Commit(); err != nil {
