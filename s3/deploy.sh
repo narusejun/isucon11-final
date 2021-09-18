fde7451f8622ae01de9379b3c0f3b5a7c4a7c95e
tohutohu <tohu.soy@gmail.com>
2021-09-18 15:43:03 +0900

    tomeru

--- s3/deploy.sh
+++ s3/deploy.sh
@@ -10,18 +10,18 @@ cd /home/isucon/webapp/go
 make build
 
 sudo systemctl restart isucholar.go
-sudo systemctl restart nginx
-sudo systemctl restart mysql
+# sudo systemctl restart nginx
+# sudo systemctl restart mysql
 
 
 # slow query logを有効化する
-QUERY="
- set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
- set global long_query_time = 0;
- set global slow_query_log = ON;
-"
+# QUERY="
+#  set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
+#  set global long_query_time = 0;
+#  set global slow_query_log = ON;
+# "
 
-echo $QUERY | sudo mysql -uroot
+# echo $QUERY | sudo mysql -uroot
 
 # log permission
 sudo chmod 777 /var/log/nginx /var/log/nginx/*
